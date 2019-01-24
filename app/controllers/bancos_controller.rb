4c23cfb108c1e52cfded002b393af2d104d03cfd
Catharina Mesquita <anacatharina@infoway-pi.com.br>
2019-01-24 15:38:52 -0300

    Adicionando permissão de usuário para acessar controllers de Banco e Saldo

--- app/controllers/bancos_controller.rb
+++ app/controllers/bancos_controller.rb
@@ -1,4 +1,5 @@
 class BancosController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_banco, only: [:show, :edit, :update, :destroy]
 
   # GET /bancos
--- app/controllers/saldos_controller.rb
+++ app/controllers/saldos_controller.rb
@@ -1,4 +1,5 @@
 class SaldosController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_saldo, only: [:show, :edit, :update, :destroy]
 
   # GET /saldos
