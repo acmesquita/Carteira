42411778afef7b81f98b051cb079d8da3b246494
Catharina Mesquita <anacatharina@infoway-pi.com.br>
2019-01-29 10:58:18 -0300

    Adicionando média dos gastos

--- app/helpers/dividendos_helper.rb
+++ app/helpers/dividendos_helper.rb
@@ -7,4 +7,11 @@ module DividendosHelper
     def valor_total_pago(dividendos)
         dividendos.select{|d| d.pago }.map{|d| d.valor}.sum rescue 0
     end
+
+    def media_de_gastos
+        dividendos = current_user.dividendos
+        total = dividendos.map{|d|d.valor}.sum
+        meses = dividendos.map{|d|d.competencia.strftime("%m/%Y") }.to_set.count
+        total / meses
+    end
 end
